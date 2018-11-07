cca9b92bde887672310b2a769856c34e561a948d
zvonko996 <zvonkosimic36@gmail.com>
2018-11-07 16:27:01 +0100

    push

---
+++ Project 2/fajl.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").slideToggle("slow");
+    }); 
+
+    $("#mes").click(function () {
+        $("#me").slideToggle("slow");
+    }); 
+    $("#mess").click(function () {
+        $("#mee").slideToggle("slow");
+    }); 
+    $("#messs").click(function () {
+        $("#meee").slideToggle("slow");
+    }); 
+});
+
+
+
